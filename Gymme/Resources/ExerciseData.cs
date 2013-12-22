e77907cc25cb4a00cf8982843855f24834807f02
dimati93 <none@none>
2013-12-22 21:28:11 +0200

    - loading of localized perset - order perset in alphabet order

--- Gymme/Resources/ExerciseData.cs
+++ Gymme/Resources/ExerciseData.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Windows;
 using System.Windows.Resources;
 using System.Xml.Linq;
+
 using Gymme.Data.AuxModels;
-using Gymme.Data.Interfaces;
 
 namespace Gymme.Resources
 {
@@ -31,9 +32,7 @@ namespace Gymme.Resources
 
         public void LoadData()
         {
-            Uri uri = new Uri("Gymme;component/Resources/ExerciseData.xml", UriKind.Relative);
-            StreamResourceInfo info = Application.GetResourceStream(uri);
-
+            StreamResourceInfo info = GetLocalizedResourceStream();
             XElement root = XElement.Load(info.Stream);
             
             try
@@ -58,9 +57,13 @@ namespace Gymme.Resources
                             throw new NullReferenceException("Attribute 'Name' not founded");
                         }
 
-                        return new PersetExercise {Category = categotyName, Name = name.Value};
+                        return new PersetExercise { Category = categotyName, Name = name.Value };
                     });
-                }).Select((x, i) => { x.Index = i; return x;}).ToList();
+                })
+                .OrderBy(x => x.Category)
+                .ThenBy(x => x.Name)
+                .Select((x, i) => { x.Index = i; return x; })
+                .ToList();
 
                 IsDataLoaded = true;
             }
@@ -68,6 +71,32 @@ namespace Gymme.Resources
             {
                 MessageBox.Show("Error in xml file parse.");
             }
+        }
+
+        private StreamResourceInfo GetLocalizedResourceStream()
+        {
+            try
+            {
+                string cult = CultureInfo.CurrentUICulture.Name;
+                StreamResourceInfo info = Application.GetResourceStream(GetLocalizedResourceUri(cult));
+                if (info != null)
+                {
+                    return info;
+                }
+            }
+            catch (Exception) {}
+
+            return Application.GetResourceStream(GetDefaultResourceUri());
+        }
+
+        private Uri GetDefaultResourceUri()
+        {
+            return new Uri("Gymme;component/Resources/ExerciseData.xml", UriKind.Relative);
+        }
+
+        private Uri GetLocalizedResourceUri(string locationAbr)
+        {
+            return new Uri(string.Format("Gymme;component/Resources/ExerciseData.{0}.xml", locationAbr), UriKind.Relative);
         }
     }
 }
